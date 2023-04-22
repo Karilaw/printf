dc3086f2cae6e99cdc0e48ffdcde95578c877b4e
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-04-22 15:09:41 -0700

    Modified unsigned integer

--- 0-printf.c
+++ 0-printf.c
@@ -1,19 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
-
 /**
  * _printf - function that produces output according to a format
  * @format: a character string  composed of zero or more directives
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int counter = 0;
 
 	va_start(args, format);
-
 	while (*format)
 	{
 		if (*format != '%')
@@ -26,7 +23,7 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == 'c')
 			{
-				char c = (char) va_arg(args,int);
+				char c = (char) va_arg(args, int);
 				putchar(c);
 				counter++;
 			}
@@ -35,17 +32,22 @@ int _printf(const char *format, ...)
 				char *s = va_arg(args, char *);
 
 				while (*s)
-				{	
+				{
 					putchar(*s);
 					counter++;
 					s++;
 
 				}
-
 			}
 			else if (*format == '%')
-			{	
+			{
 				putchar('%');
+				counter++;
+			}
+			else if(*format == 'u')
+			{
+				unsigned int u = va_arg(args, unsigned int);
+				putchar(u);
 				counter++;
 			}
 			else
@@ -53,13 +55,9 @@ int _printf(const char *format, ...)
 				putchar(*format);
 				counter += 2;
 			}
-
 	}
 		format++;
-
 	}
 	va_end(args);
 	return (counter);
-
-
 }
