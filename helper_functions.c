ce800f1337385835342a3f937e9afef2b85c0554
gmugabo <mugabogad88@gmail.com>
2023-04-25 12:08:53 -0700

    added _uitoi function

--- helper_functions.c
+++ helper_functions.c
@@ -38,3 +38,44 @@ char *conv_to_binary(unsigned int num)
 
 
 }
+
+char *_uitoa_base(unsigned int n, unsigned int base)
+{
+	char *s;
+	char array_digits[] = "0123456789abcdef";
+	int i = 0;
+	int j;
+	char temp;
+
+	if (n == 0)
+	{
+		s = malloc(sizeof(char) * 2);
+		if (s == NULL)
+			return (NULL);
+		s[0] = '0';
+		s[1] = '\0';
+		return (s);
+	}
+
+	s = malloc(sizeof(char) * 33);
+	if (s == NULL)
+		return (NULL);
+
+	while (n > 0)
+	{
+		s[i] = array_digits[n % base];
+		n /= base;
+		i++;
+	}
+	s[i] = '\0';
+
+	for (j = 0; j < i / 2; j++)
+	{
+		temp = s[j];
+		s[j] = s[i - j - 1];
+		s[i - j - 1] = temp;
+	}
+	return (s);
+}
+
+
